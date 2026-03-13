5b4aae9bbd180ef55b46cfbb98bbd67b419ef1da
Amsha67 <eric.schmolzlin@gmail.com>
2026-03-13 12:00:26 +0100

    midi 13/03

--- traitement.php
+++ traitement.php
@@ -1,17 +1,24 @@
 <?php
-
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // 1. Verification du champ "honeypot"
+    if (!empty($_POST["bip_boop_robot"])) {
+        die("Erreur : Robot détecté.");
+    }
 
-$nom = htmlspecialchars($_POST["nom"]);
+    // 2. Vérification du calcul aléatoire
+    $reponseUtilisateur = intval($_POST["verif_humain"]);
+    $reponseAttendue = intval($_POST["total_secret"]);
 
-echo "<h1>Bonjour $nom </h1>";
+    if ($reponseUtilisateur !== $reponseAttendue) {
+        die("Erreur : Mauvais calcul. Vous n'êtes pas humain !");
+    }
 
-echo "<p>Le formulaire a bien été envoyé.</p>";
-
-} else {
-
-echo "Aucune donnée reçue.";
+    $nom = htmlspecialchars($_POST["nom"]);
+    echo "<h1>Bonjour $nom</h1><p>Demande transmise avec succès.</p>";
 
 }
 
-?> 
+
+
+
+?>
